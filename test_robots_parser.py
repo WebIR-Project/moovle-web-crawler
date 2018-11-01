9a7f11d11b6caf743772df5dfd0e3df734da1c2f
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2018-11-01 22:35:20 +0700

    added test get_root_url

---
+++ test_robots_parser.py
@@ -0,0 +1,15 @@
+import unittest
+from urllib.parse import urlparse
+from robots_parser import RobotParser
+
+class TestGetRootUrl(unittest.TestCase):
+
+    def setUp(self):
+        self.robots_parser = RobotParser(user_agent='unittest')
+
+    def test_normal_url(self):
+        parsed_url = urlparse('http://www.testurl.com/mytest/1')
+        self.assertEqual(self.robots_parser.get_root_url(parsed_url), 'http://www.testurl.com')
+
+if __name__ == '__main__':
+    unittest.main()
