5207379130326794a697e8aea234b180255ea8b4
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2018-11-05 00:02:23 +0700

    updated exception

--- web_crawler.py
+++ web_crawler.py
@@ -36,44 +36,61 @@ def t_print(thread_name, msg):
 def worker():
     global db, sch, rp
     t_name = threading.current_thread().getName()
+    t_print(t_name, 'Started')
     d = downloader.Downloader(use_proxies=True)
     while True:
         lock.acquire()
         if sch.get_queue_length() == 0:
-            break
+            t_print(t_name, 'Nothing to do')
+            lock.release()
+            continue
         url = sch.dequeue()
         lock.release()
+        try:
+            t_print(t_name, f'Downloading {url}')
 
-        t_print(t_name, f'Downloading {url}')
+            html = None
+            while True:
+                try:
+                    html = d.get_page(url)
+                    break
+                except downloader.PageNotFound:
+                    break
+                except downloader.NetworkError:
+                    t_print(t_name, f'Retrying download {url}')
+                    pass
+            parsed_html = analyzer.parse_html(html)
+            parsed_url = urlparse(url)
+            hostname = parsed_url.netloc
+            links = [analyzer.normalize_url(hostname, link) for link in analyzer.extract_links(parsed_html)]
+            lock.acquire()
+            if html is not None:
+                t_print(t_name, f'Saving {url}')
+                save_page(url, parsed_html, html, links)
+            else:
+                t_print(t_name, f'Cannot download {url}')
+            for link in [link for link in links if analyzer.is_html_page(link) and rp.is_allowed(link) and len(urlparse(link).path.split('/')) <= 20]:
+                sch.enqueue(link)
+            sch.visited(url)
+            lock.release()
+        except Exception:
+            sch.enqueue(url)            
 
-        html = None
-        while True:
-            try:
-                html = d.get_page(url)
-                break
-            except downloader.PageNotFound:
-                break
-            except downloader.NetworkError:
-                pass
-        parsed_html = analyzer.parse_html(html)
-        parsed_url = urlparse(url)
-        hostname = parsed_url.netloc
-        links = [analyzer.normalize_url(hostname, link) for link in analyzer.extract_links(parsed_html)]
-
-        lock.acquire()
-        if html is not None:
-            t_print(t_name, f'Saving {url}')
-            save_page(url, parsed_html, html, links)
-        else:
-            t_print(t_name, f'Cannot download {url}')
-        for link in [link for link in links if analyzer.is_html_page(link) and rp.is_allowed(link) and len(urlparse(link).path.split('/')) <= 20]:
-            sch.enqueue(link)
-        sch.visited(url)
-        lock.release()
+    t_print(t_name, 'Done')
 
 config = read_config()
 threads = []
+print('Starting...')
 for i in range(config['n_thread']):
     t = threading.Thread(name=i, target=worker)
+    t.daemon = True
     threads.append(t)
     t.start()
+while True:
+    all_terminated = True
+    for t in threads:
+        if t.is_alive():
+            all_terminated = False
+            break
+    if all_terminated:
+        break
