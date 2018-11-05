4dd19fb857ec7626b73db06d55b299614a2b8880
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2018-11-05 18:42:20 +0700

    check url scheme

--- analyzer.py
+++ analyzer.py
@@ -25,11 +25,14 @@ def is_html_page(url):
     parsed_url = urlparse(url)
     result = True
     if parsed_url.path != '':
-        path = [item for item in parsed_url.path.split('/') if item != '']
-        if len(path) > 0:
-            last = path[-1]
-            splitted_last = last.split('.')
-            ext = splitted_last[-1]
-            if len(splitted_last) > 1 and ext != 'html' and ext != 'htm' and ext != 'php':
-                result = False
+        if parsed_url.scheme != 'http' and parsed_url.scheme != 'https':
+            result = False
+        else:
+            path = [item for item in parsed_url.path.split('/') if item != '']
+            if len(path) > 0:
+                last = path[-1]
+                splitted_last = last.split('.')
+                ext = splitted_last[-1]
+                if len(splitted_last) > 1 and ext != 'html' and ext != 'htm' and ext != 'php':
+                    result = False
     return result
