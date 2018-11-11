2b6f84873b5980d6667ef3ac2913c4c95f2decaa
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2018-11-11 14:08:36 +0700

    created image extractor module

---
+++ image_extractor.py
@@ -0,0 +1,28 @@
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup as bs
+from pymongo import MongoClient
+
+c = MongoClient()
+db = c.moovle
+
+def extract_images(url, soup):
+    image_tags = soup.find_all('img')
+    image_urls = [urljoin(url, item.get('src')) for item in image_tags]
+    return image_urls
+
+if __name__ == '__main__':
+    count = 0
+    while True:
+        docs = list(db.pages.aggregate([{'$match': {'extracted': {'$exists': False}}}, {'$limit': 1}]))
+        if len(docs) == 0:
+            break
+        doc = docs[0]
+        soup = bs(doc['html'], 'html.parser')
+        image_urls = extract_images(doc['url'], doc)
+        db.pages.update({'url': doc['url']}, {'$set': {'images': image_urls, 'extracted': True}})
+        count += 1
+        if count % 100 == 0:
+            print('.', end='', flush=True)
+    print()
+    print('cleaning...')
+    db.pages.update({}, {'$unset': {'extracted': True}}, multi=True)
